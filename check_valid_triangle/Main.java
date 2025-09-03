a1815e236094b9f5b6d585874e49628762e1c2eb
vishaldeshmukh34 <vishaldeshmukh7972@gmail.com>
2025-09-03 19:42:29 +0530

    Check Triangle is Valid or Not

---
+++ check_valid_triangle/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    
+    public static void main(String[] args){
+    
+    int a = 60;
+    int b = 60;
+    int c = 60;
+    
+    int sum = a + b + c ;
+    
+    if( sum == 180)  {
+      System.out.println("This is Valid Triangle");
+    } else  { 
+     System.out.println("This is Not Valid Triangle");
+        
+    }
+ 
+    }
+}
