cdda4f80aa39f9435463651a1579c37ad6e53a36
vishaldeshmukh34 <vishaldeshmukh7972@gmail.com>
2025-09-03 22:01:49 +0530

    Added voter program Main.java

---
+++ voter_program/Main.java
@@ -0,0 +1,13 @@
+package voter_program;
+
+public class Main {
+     public static void main(String[] args) {
+        int age = 21 ;
+
+        if(age >= 18) {
+            System.out.println("vishal is Ealigiable For Vote");
+        }else{
+            System.out.println("vishal is Not Ealigiable For vote");
+        }
+     }
+}
