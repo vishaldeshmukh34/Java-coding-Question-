658cc8158cce4529854cdcb3d1217f4e80d656b7
vishaldeshmukh34 <vishaldeshmukh7972@gmail.com>
2025-09-05 16:38:10 +0530

    this is condition if

---
+++ Condition_if/Condition_if.java
@@ -0,0 +1,20 @@
+package Condition_if;
+
+import java.util.Scanner;
+
+public class Condition_if {
+    
+    public static void main(String[] args) {
+        int pwd;
+        System.out.print("Enter Password:=");
+        Scanner obj = new Scanner(System.in);
+        pwd = obj.nextInt();
+
+        if(pwd==7972){
+            System.out.println("My Name : Vishal Deshmukh");
+            System.out.println("My Age : 21");
+            System.out.println("My Contact : 90907363637");
+            System.out.println("My Adress : Pune ");
+        }
+    }
+}
