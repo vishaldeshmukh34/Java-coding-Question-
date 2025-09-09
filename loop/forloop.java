f1b4b46c78a4b7ad807ad3b0b04e57027beccbd1
vishaldeshmukh34 <vishaldeshmukh7972@gmail.com>
2025-09-09 16:00:34 +0530

    Added login feature

---
+++ loop/forloop.java
@@ -0,0 +1,11 @@
+public package loop;
+
+class forloop {
+
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
