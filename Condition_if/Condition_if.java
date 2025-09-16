2e56abc24b2d88dacaf679429d47cd92fa01bfc1
vishaldeshmukh34 <vishaldeshmukh7972@gmail.com>
2025-09-16 10:49:08 +0530

    Added if_ladder.java program

--- Condition_if/Condition_if.java
+++ Condition_if/Condition_if.java
@@ -12,9 +12,13 @@ public class Condition_if {
 
         if(pwd==7972){
             System.out.println("My Name : Vishal Deshmukh");
+            System.out.println("My College : BBMJ");
             System.out.println("My Age : 21");
             System.out.println("My Contact : 90907363637");
             System.out.println("My Adress : Pune ");
+        }
+        else {
+            System.out.println("Plese Enter Correct Possword ");
         }
     }
 }
---
+++ Condition_if/if_ladder.java
@@ -0,0 +1,51 @@
+
+ package Condition_if;
+
+public class if_ladder {
+    public static void main(String[] args) {
+        int a = 10;
+        
+
+        if(a == 1) 
+        {
+           System.out.println("TEN");
+        }
+        else if (a == 2)
+        {
+            System.out.println("TWO");
+        }
+        else if (a == 3)
+        {
+            System.out.println("THREE");
+        }
+        else if (a == 4)
+        {
+            System.out.println("FOUR");
+        }
+        else if (a == 5)
+        {
+            System.out.println("FIVE");
+        }
+        else if (a == 6)
+        {
+            System.out.println("SIX");
+        }
+        else if (a == 7)
+        {
+            System.out.println("SEVEN");
+        }
+        else if (a == 8)
+        {
+            System.out.println("EIGHT");
+        }
+        else if (a == 9)
+        {
+            System.out.println("NING");
+        }
+        else if(a == 10)
+        {
+            System.out.println("TEN");
+        }
+        
+    }
+}
