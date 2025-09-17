be5341fb7efcb9f1c97ad9aff0a6b55520c378f2
vishaldeshmukh34 <vishaldeshmukh7972@gmail.com>
2025-09-17 19:30:58 +0530

    Added Q4.java program to compare two numbers

---
+++ condition_else_if/Q9.java
@@ -0,0 +1,16 @@
+public class Q9 {
+    public static void main(String[] args) {
+           int a = 100 ;
+        int b = 78 ;
+        int c = 78 ;
+        
+        if(a+b>c && b+c>a && c+a>b )
+        {
+               System.out.println("IS VALID TRIANGLE");
+        }
+        else
+        {
+             System.out.println("IS  NOT VALID TRIANGLE");
+        }
+    }
+}
