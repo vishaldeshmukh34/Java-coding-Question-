0176b9a7eb983a2a29172f50f02d2adbc4db5e73
vishaldeshmukh34 <vishaldeshmukh7972@gmail.com>
2025-09-18 19:18:12 +0530

    Added 11 Java programs

---
+++ switch case/C.java
@@ -0,0 +1,27 @@
+public class C {
+    public static void main(String[] args) {
+        //Grade System
+// Take marks (0–100) and print grade using switch.
+
+// 90–100 → A
+
+// 80–89 → B
+
+// 70–79 → C
+
+// 60–69 → D
+
+// Below 60 → Fail
+  int a = 23 ;
+
+  switch (a) {
+    case 90–100 :
+        System.out.println("90");
+        break;
+  
+    default:
+        break;
+  }
+
+    }
+}
