3fd04b053706755204c62d28dd39414c6bd87e89
vishaldeshmukh34 <vishaldeshmukh7972@gmail.com>
2025-09-19 13:20:52 +0530

    Added Traffic Light System program

--- switch case/E.java
+++ switch case/E.java
@@ -1,7 +1,9 @@
 public class E {
    
       public static void main(String[] args) {
-         
+        
+          //Q.E] Vowel or Consonant Input a character and check whether it is a vowel or consonant using switch.
+
          char cha = 'A';
        switch(cha)
        {
@@ -10,7 +12,7 @@ public class E {
            case 'I' : System.out.println("I IS VOWEL");break;
            case 'O' : System.out.println("O IS VOWEL");break;
            case 'U' : System.out.println("U IS VOWEL");break;
-           default: System.out.println("NO VOWEL IN ");break;
+           default: System.out.println("Consonant ");break;
        }
     }
 }
