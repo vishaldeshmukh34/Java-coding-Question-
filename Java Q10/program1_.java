5bddd5d40f06fa06e5cd713df4b8fdd127e33c6e
vishaldeshmukh34 <vishaldeshmukh7972@gmail.com>
2025-09-20 19:28:58 +0530

    first commit

---
+++ Java Q10/program1_.java
@@ -0,0 +1,10 @@
+public class program1_ {
+    public static void main(String[] args) {
+        // WAP to convert temperature from Celsius to Fahrenheit. 
+
+        int Celsius = 30 ;
+
+        double Fahrenheit = (Celsius * 9/5) + 32.;
+        System.out.println(Fahrenheit);
+    }
+}
---
+++ Java Q10/program2_.java
@@ -0,0 +1,9 @@
+public class program2_ {
+    public static void main(String[] args) {
+        // WAP to convert temperature from Fahrenheit to Celsius.
+         int Fahrenheit = 20 ;
+
+         double Celsius = (Fahrenheit * 9 / 32.);
+         System.out.println(Celsius);
+    }
+}
